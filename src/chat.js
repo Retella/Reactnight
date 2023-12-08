c64b9fb5c411c7ae7ba463fb034c91580465e14e
Retella <agtobarruela@gmail.com>
2023-12-09 00:07:28 +0100

    nowMyWeb

---
+++ src/chat.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+ useState
+} from "react";
+import {
+ useEffect
+} from "react";
+
+export default function ChatApp() {
+
+ const demoMsg = [
+  ["Hi", "Obama"],
+  ["Bonjorno", "Francis"]
+ ]
+
+ const [messages, changeMsg] =
+ useState(demoMsg);
+ const [username, setName] =
+ useState("guest");
+
+ const addMsg = () => {
+  changeMsg(messages + [document
+   .getElementById("textBox"),
+   username
+  ]);
+ }
+
+ const msgArr = messages.map((
+   m) =>
+  <
+  div id = "msg" > < p id =
+  "msgText" > {
+   m[0]
+  } <
+  /p> <p id="msgName"><strong>{m[1]}</strong >
+  <
+  /p>< /div > );
+
+ return <div id = "chat" > <
+  div id = "messages" > {
+   msgArr
+  } < /div > <
+ input id = "textBox" /
+  >
+  <
+  button onClick = {
+   addMsg
+  } > Send < /button> < /
+ div >
+}
