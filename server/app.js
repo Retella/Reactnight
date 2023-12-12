665faa3de0dc7ffecac8c1d3adee36732485f4be
Retella <agtobarruela@gmail.com>
2023-12-12 09:22:08 +0100

    sin api

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.json({
     message: 'Hello world',
   });
