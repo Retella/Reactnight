0eb21b006fa5c67bf8ae61f12149b9ee9e3bb615
Retella <agtobarruela@gmail.com>
2023-12-20 23:49:54 +0100

    construyendo matchmaking

--- server/app.js
+++ server/app.js
@@ -17,11 +17,25 @@ const io = socketIo(server, {
 });
 
 const idList = {}
+const readyList = {}
+const rolesObj = {}
+
+function logger() {
+ console.log(`${"-".repeat(50)}`)
+ console.log(`Players: ${JSON.stringify(idList)}`)
+ console.log(`ReadyList: ${JSON.stringify(readyList)}`)
+ console.log(`HackerList: ${JSON.stringify(rolesObj)}`)
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
 
 io.on('connection', (socket) => {
     socket.on("newUser", (data) => {
         idList[socket.id] = data["user"]
-        console.log(idList)
+        readyList[socket.id] = false
+        logger()
 
         io.emit("newUserResponse",
 {usernames:Object.values(idList),ids:Object.keys(idList)})
@@ -32,9 +46,39 @@ io.on('connection', (socket) => {
 {text:message["text"], author:idList[socket.id], index:Object.keys(idList).indexOf(socket.id)});
     });
 
+    socket.on("sendReady", (data) => {
+     readyList[socket.id] = data["bool"]
+
+     for (let i in Object.values(readyList)) {
+      if (!Object.values(readyList)[i]) {
+       return
+      }
+     }
+
+     for (const i in Object.keys(idList)) {
+      rolesObj[Object.keys(idList)[i]] = false
+     }
+
+     const hackerNum = Math.round(Object.keys(rolesObj).length/2 - 1)
+     let hackIt = 0
+     while (hackIt < hackerNum) {
+      const daIdx = getRndInteger(0, Object.keys(rolesObj).length)
+      if (!rolesObj[Object.keys(idList)[daIdx]]) {
+       rolesObj[Object.keys(idList)[daIdx]] = true
+       hackIt += 1
+      }
+     }
+
+     logger()
+
+     io.emit("gameStart", {roles:rolesObj})
+})
+
     socket.on('disconnect', () => {
       console.log('user disconnected');
+      delete readyList[socket.id]
       delete idList[socket.id]
+      delete rolesObj[socket.id]
 
       io.emit("newUserResponse",
 {usernames:Object.values(idList),ids:Object.keys(idList)})
--- src/decision.js
+++ src/decision.js
@@ -2,8 +2,59 @@ import React from "react";
 import {useState} from "react";
 import {useEffect} from "react";
 
-export default function ChooseApp() {
 
-return (<div id="chooseDiv">Yo</div>)
+export default function ChooseApp(props) {
 
+const [isHacker, setHacker] = useState(false)
+const [choosing, setChoosing] = useState(true)
+const [started, setStarted] = useState(false)
+const [ready, setReady] = useState(false)
+
+useEffect(() => {
+ props.socket.on("gameStart", (data) => {
+  setStarted(true)
+  setHacker(data["roles"][props.socket.id])
+ })
+})
+
+let hackcss = {opacity:0.2}
+let agentcss = {opacity:0.2}
+
+if (choosing) {
+ agentcss = {opacity: 1}
+ if (isHacker) {
+  hackcss = {opacity: 1}
+ }
+}
+
+const hacker = <div id="hack" style={hackcss}><p><font size='20'><strong>
+HACKEAR</strong></font></p></div>
+
+const agent = <div id="secure" style={agentcss}><p><font size='20'><strong>
+ASEGURAR</strong></font></p></div>
+
+if (started) {
+return (
+<div id="chooseDiv">
+{hacker}
+{agent}
+</div>
+)
+}
+
+const text = (ready)? "Preparado":"No preparado"
+
+const readyColor = (ready)? "Orange":"LightSalmon"
+
+return (
+ <div id="readyDiv" style={
+{
+ backgroundColor: readyColor,
+}
+} onClick={()=>{
+props.socket.emit("sendReady", {bool:!ready})
+setReady(!ready)
+}}
+><strong><font size='20'>{text}</font></strong></div>
+)
 }
